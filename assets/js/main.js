941c8940502e440f120b09195bcee6054b7d6418
Sam Goddard <hello@samgoddard.co.uk>
2018-02-27 13:23:03 +0000

    barefoot module added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,13 @@
 import bindToggles from './modules/toggle';
 import bindModals from './modules/modals';
 import bindAjaxForms from './modules/ajaxForm';
+// import BareFoot from './vendor/barefoot';
 
 function init(){
     bindToggles();
     bindModals();
     bindAjaxForms();
+    // BareFoot();
 }
 
 window.addEventListener('DOMContentLoaded', init);
