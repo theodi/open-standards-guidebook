cad6e165ee44e254fd5a83695b801be62ea05cb9
Tom Davies <tom@tomdavies.net>
2018-02-28 10:41:51 +0000

    add event tracking on: external links; toc links; downloads

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,15 @@
 import bindToggles from './modules/toggle';
 import bindModals from './modules/modals';
 import bindAjaxForms from './modules/ajaxForm';
-import littlefoot from 'littlefoot';
+import bindEventTracking from './modules/eventTracking';
+import bindFootnotes from 'littlefoot';
 
 function init(){
     bindToggles();
     bindModals();
     bindAjaxForms();
-    littlefoot();
+    bindEventTracking();
+    bindFootnotes();
 }
 
 window.addEventListener('DOMContentLoaded', init);
