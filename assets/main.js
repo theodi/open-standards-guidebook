a087b7162e2339079b779376cac4d8fccde32ca8
Tom Davies <tom@tomdavies.net>
2019-07-09 12:48:11 +0100

    Rejig assets dir structure to work better with vue-cli

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,5 @@
+import './components/main.scss';
+
 import bindFootnotes from 'littlefoot';
 import bindToggles from './modules/toggle';
 import bindModals from './modules/modals';
@@ -10,6 +12,9 @@ function init() {
     bindAjaxForms();
     bindEventTracking();
     bindFootnotes();
+
+    document.documentElement.classList.remove('js-loading');
+    document.documentElement.classList.add('js-loaded');
 }
 
 window.addEventListener('DOMContentLoaded', init);
