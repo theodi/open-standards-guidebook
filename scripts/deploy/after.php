5d99c4ecdab9f836e700f057ee647d1a387be40f
Tom Davies <tom@tomdavies.net>
2019-07-10 11:39:04 +0100

    remove script cruft

--- scripts/deploy/after.php
+++
@@ -1,8 +0,0 @@
-<?php
-  echo shell_exec('php craft clear-caches/compiled-templates');
-  echo shell_exec('php craft clear-caches/data');
-  echo shell_exec('php craft clear-caches/temp-files');
-  echo shell_exec('php craft clear-caches/template-caches');
-  echo shell_exec('php craft clear-caches/upper-purge-all');
-  echo shell_exec('php craft migrate/all');
-?>
