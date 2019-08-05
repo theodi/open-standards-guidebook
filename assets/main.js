84ad34bc812cf1f7388d867de853285ad492f412
Tom Davies <tom@tomdavies.net>
2019-08-05 16:32:30 +0100

    lintage

--- assets/main.js
+++ assets/main.js
@@ -1,20 +1,20 @@
-import './styles/main.scss';
+import "./styles/main.scss";
 
-import bindFootnotes from 'littlefoot';
-import bindToggles from './js/toggle';
+import bindFootnotes from "littlefoot";
+import bindToggles from "./js/toggle";
 import bindSearch from "./js/search/index";
-import bindEventTracking from './js/eventTracking';
+import bindEventTracking from "./js/eventTracking";
 
-function init() {
-    if (APP.CONFIG.ALGOLIA) {
-      APP.SEARCH = bindSearch(APP.CONFIG.ALGOLIA); // eslint-disable-line no-param-reassign
-    }
-    bindToggles();
-    bindEventTracking();
-    bindFootnotes();
+function init(APP) {
+  if (APP.CONFIG.ALGOLIA) {
+    APP.SEARCH = bindSearch(APP.CONFIG.ALGOLIA); // eslint-disable-line no-param-reassign
+  }
+  bindToggles();
+  bindEventTracking();
+  bindFootnotes();
 
-    document.documentElement.classList.remove('js-loading');
-    document.documentElement.classList.add('js-loaded');
+  document.documentElement.classList.remove("js-loading");
+  document.documentElement.classList.add("js-loaded");
 }
 
-window.addEventListener('DOMContentLoaded', init);
+window.addEventListener("DOMContentLoaded", () => init(window.APP));
